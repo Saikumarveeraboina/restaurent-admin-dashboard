0dd6ddeb6327294be7ee217f4c28806e9422778b
Saikumar <saikumar96030433@gmail.com>
2026-02-06 16:32:05 +0530

    Update order.routes.js

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const c = require("../controllers/order.controller");
 const validateOrder = require("../validators/order.validator");
-
 router.post("/", validateOrder, c.createOrder);
 router.get("/", c.getOrders);
 router.get("/:id", c.getOrderById);
