806f90c783d63821f14e99f8fc20877234e15abf
Saikumar <saikumar96030433@gmail.com>
2026-02-06 16:31:56 +0530

    Update order.routes.js

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -3,8 +3,6 @@ const c = require("../controllers/order.controller");
 const validateOrder = require("../validators/order.validator");
 
 router.post("/", validateOrder, c.createOrder);
-
-
 router.get("/", c.getOrders);
 router.get("/:id", c.getOrderById);
 router.post("/", c.createOrder);
