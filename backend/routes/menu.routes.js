4b2dbed2219e1c9bd1d63b1c432b3925468ce2fa
Saikumar <saikumar96030433@gmail.com>
2026-02-06 16:32:28 +0530

    Update menu.routes.js

--- backend/routes/menu.routes.js
+++ backend/routes/menu.routes.js
@@ -1,21 +1,15 @@
 const router = require("express").Router();
 const c = require("../controllers/menu.controller");
 
-// GET
 router.get("/", c.getMenu);
 router.get("/search", c.searchMenu);
 router.get("/:id", c.getMenuById);
 
-// POST
 router.post("/", c.createMenu);
 
-// PUT
 router.put("/:id", c.updateMenu);
 
-// DELETE
 router.delete("/:id", c.deleteMenu);
-
-// PATCH
 router.patch("/:id/availability", c.toggleAvailability);
 
 module.exports = router;
